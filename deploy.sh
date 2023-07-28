24bcbfd31b70be0445885c6d53bd432038376380
Luis <luisdanielhernandezlopez@gmail.com>
2023-07-27 23:39:01 -0500

    font

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ mv  nvim ~/.config/
 mv  nvim.local ~/.local/share/nvim 
 mv  wallpaper ~/.config/ 
 mv  suckless ~/.config/ 
+mv  JetBrains-Mono-Nerd-Font-Complete-Bold.ttf ~/.local/share/fonts/
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+
